e59c5079c53f782667cce8d493e65d18259305ae
shweta1824 <sss2516@gmail.com>
2024-05-07 15:17:29 +0530

    profile card

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
@@ -6,39 +6,16 @@ import { AuthService } from 'src/app/Services/auth.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   email: string = '';
   name: string = '';
 
   constructor(private authService: AuthService) { }
 
-  ngOnInit() {
-    // Check if user is authenticated
-    if (this.authService.isAuthenticated()) {
-      // Retrieve user email and name
-      const storedEmail = localStorage.getItem('userEmail');
-      const storedName = localStorage.getItem('userName');
-
-      if (storedEmail && storedName) {
-        this.email = storedEmail;
-        this.name = storedName;
-      } else {
-        // Fetch user details if not stored in local storage
-        this.authService.getUserEmail().subscribe(
-          (res: any) => {
-            this.email = res.email;
-            this.name = res.name;
-            // Store user data in local storage
-            localStorage.setItem('userEmail', this.email);
-            localStorage.setItem('userName', this.name);
-          },
-          (error) => {
-            console.error('Error fetching user data:', error);
-          }
-        );
-      }
-    } else {
-      console.error('User not authenticated');
-    }
+  async ngOnInit() {
+   await this.authService.getUserEmail().then((res:any)=> {
+    this.email = res.email
+    this.name=res.name
+    })
   }
 }
